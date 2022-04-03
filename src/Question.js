ffc9bd9d0b844dd6ebda05139c4efb9c2bdf069f
Anna Shishkova <asshishkova@gmail.com>
2022-04-03 14:05:14 +0300

    questions one by one

--- src/Question.js
+++ src/Question.js
@@ -5,26 +5,48 @@ import axios from 'axios';
 import ReactHtmlParser from 'react-html-parser';
 
 function Question() {
-  const [questions, loadQuestions] = useState({results: []});
+  const [questions, setQuestions] = useState({results: []});
+  const [questionIndex, setQuestionIndex] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState({question: ""});
+  const [answers, setAnswers] = useState([]);
 
   useEffect(() => {
     axios.get("https://opentdb.com/api.php?amount=10").then((response) => {
-      loadQuestions(response.data);
+      const questions = response.data.results;
+      const questionIndex = 0;
+      const currentQuestion = questions[questionIndex];
+      const answers = [currentQuestion.correct_answer].concat(currentQuestion.incorrect_answers);
+      setCurrentQuestion(currentQuestion);
+      setAnswers(answers);
+      setQuestions(questions);
     });
   }, []);
 
   const navigate = useNavigate();
   const location = useLocation();
 
+  const nextQuestion = (nextQuestionIndex) => {
+    if (nextQuestionIndex === questions.length) {
+      navigate("/", location)
+    } else {
+      const currentQuestion = questions[nextQuestionIndex];
+      const answers = [currentQuestion.correct_answer].concat(currentQuestion.incorrect_answers);
+      setCurrentQuestion(currentQuestion);
+      setAnswers(answers);
+      setQuestionIndex(nextQuestionIndex);
+    }
+  }
+
   return (
     <div className="game">
       <p>Good luck {location.state.name}</p>
+      <h3>{ReactHtmlParser(currentQuestion.question)}</h3>
       <ol>
-        {questions.results.map(question => (
-          <li>{ReactHtmlParser(question.question)}</li>
+        {answers.map(answer => (
+          <li><button onClick={() => nextQuestion(questionIndex + 1)}>{ReactHtmlParser(answer)}</button></li>
         ))}
       </ol>
-      <button className="btn-gradient" onClick={() => navigate("/", location)}>back</button>
+      <button onClick={() => navigate("/", location)}>back</button>
     </div>
   )
 }
